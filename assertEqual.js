02feed3c929a44c186674cf55f26ca8431a7b059
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-10-26 17:25:12 -0700

    simplified code by adding else and removing 2nd if loop

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
+  } else (actual !== expected);
     console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
 };
  
 assertEqual("Lighthouse Labs", "Bootcamp");
