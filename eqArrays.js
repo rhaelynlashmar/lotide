bbaa092fa4753cf87b6686af048f0494445d158e
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-10-27 23:36:34 -0700

    removed an incorrect comment

--- eqArrays.js
+++ eqArrays.js
@@ -26,5 +26,5 @@ eqArrays([1, 2, 3], [3, 2, 1]);
 eqArrays(['1', '2', '3'], ['1', '2', '3']);
 eqArrays(['1', '2', '3'], ['1', '2', 3]);
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); 
