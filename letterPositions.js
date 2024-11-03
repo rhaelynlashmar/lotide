bf4d229dc36f62136947cf1cb8045eb4057b62c2
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-02 18:49:54 -0700

    Created Function and appropriate tests

---
+++ letterPositions.js
@@ -0,0 +1,47 @@
+// create a function that returns an object
+const letterPositions = function (sentence) {
+   const results = {};
+    // Loop through the string, checking each character
+    for (let i = 0; i < sentence.length; i++) {
+      // If the character is not a space, check if it is already a key in the object
+      if (sentence[i] !== " ") {
+        if (results[sentence[i]] === undefined) {
+          results[sentence[i]] = [i];
+        } else {
+          results[sentence[i]].push(i);
+        }
+      }
+        //if we have not encountered the letter yet add a key value pair to results with the letter as the key and an array with index as the value
+
+        //else we have already encounter this letter already, add the index to the existing array
+      
+    }
+    // if it is push the current index to the array that is its value 
+    //if not, add it as a key to the object with a value of an array containing the current index
+   return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello").o, [4]);
