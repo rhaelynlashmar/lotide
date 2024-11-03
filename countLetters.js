038431e859ac4a48f0b10e3f46564217c6378bc1
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-02 19:07:56 -0700

    Figured out how to use assertEqual to test

--- countLetters.js
+++ countLetters.js
@@ -16,4 +16,22 @@ const countLetters = function(string) {
   return results;
 };
 
+
 console.log(countLetters("lighthouse in the house"));
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(countLetters("lighthouse in the house").l, 1);
+assertEqual(countLetters("lighthouse in the house").u, 2);
+assertEqual(countLetters("lighthouse in the house").h, 4);
+assertEqual(countLetters("lighthouse in the house").e, 3);
+
+
+
+
