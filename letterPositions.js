78147131368ac21dc0dde7e71307d501a0409089
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-02 18:59:47 -0700

    changed assertEqual tests

--- letterPositions.js
+++ letterPositions.js
@@ -39,4 +39,7 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
 assertArraysEqual(letterPositions("hello").o, [4]);
