28e9beeaababdc534a62e33ce9ee080d3f4f3579
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-03 13:43:08 -0800

    created eqObjects function and tested against it

---
+++ eqObjects.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// create a function that takes in 2 objects and returns true or false, based on a perfect match
+const eqObjects = function(object1, object2) {
+  // Create variables for object1 and object2 to be shortened to
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  // First, see if the objects have the same number of keys
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+  // loop through obj1 & obj2 arrays to see if they have the same keys
+  for (const key of obj1Keys) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const candyBag =  { type: "swedish berries", color: "pink", quantity: 30};
+const candyBag2 = { color: "pink", type: "swedish berries", quantity: 30};
+const candyBag3 = {type: "sour patch kids", quantity: 30};
+eqObjects(candyBag, candyBag2);
+eqObjects(candyBag, candyBag3);
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+assertEqual(eqObjects(candyBag, candyBag2), true);
+assertEqual(eqObjects(candyBag, candyBag3), false);
+
