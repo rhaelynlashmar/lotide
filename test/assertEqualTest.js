7799cfce1030d44663f0bf7fa473437b07dfdd33
Rhaelyn Lashmar <rhaelynlashmar@gmail.com>
2024-11-16 19:25:10 -0800

    Delete test/assertEqualTest.js

--- test/assertEqualTest.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require("../assertEqual");
-
-
-console.log(assertEqual("Eat, Pray, Love", "Eat, Pray, Love"));
-console.log(assertEqual(42, 24));
-console.log(assertEqual(5 * 2, 10));
-console.log(assertEqual(1000, "1000"));
