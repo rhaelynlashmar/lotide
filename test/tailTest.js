957831311a141642d51b547d7b52aba4335d0af6
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 22:56:20 -0800

    converted testing to chai and array testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,15 @@
-const assertEqual = require("../assertEqual.js");
 const tail = require("../tail.js");
+const assert = require('chai').assert;
 
+describe("#tail", () => {
+  it("should return ['Sound', 'Brewery'] for ['Howe', 'Sound', 'Brewery']", () => {
+    assert.deepEqual(tail(["Howe", "Sound", "Brewery"]), ["Sound", "Brewery"]);
+  });
+  it("should return [4, 6, 8, 10] for [2, 4, 6, 8, 10]", () => {
+    assert.deepEqual(tail([2, 4, 6, 8, 10]), [4, 6, 8, 10]);
+  });
+  it("should return ['3', '5', '7', '9'] for ['1', '3', '5', '7', '9']", () => {
+    assert.deepEqual(tail(['1', '3', '5', '7', '9']), ['3', '5', '7', '9']);
+  });
+});
 
-const arr = ["Howe", "Sound", "Brewery"];
-const tailArray = tail(arr);
-assertEqual(tailArray.length, 2);
-assertEqual(tailArray[0], "Sound");
-assertEqual(tailArray[1], "Brewery");
-assertEqual(tailArray[1], "Wrong");
-// checking to see if my array is still 3
-assertEqual(arr.length, 3);
-// testing to see if the empty array comes back an empty array
-const emptyArr = tail([]);
-assertEqual(emptyArr.length, 0);
