9f01e124dd2a0ff3fa3af3a80d3792b60dc1ebbb
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 21:15:40 -0800

    added require links to eqArrays and exported links for tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,5 +8,6 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [1,2,4]);
+module.exports = assertArraysEqual;
+
+
