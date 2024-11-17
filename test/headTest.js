78fe3b42298c60ca50d4969e78945db3839d30ab
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 20:29:16 -0800

    altered to add assertEqual function

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,6 @@
 const head = require("../head.js");
+const assertEqual = require("../assertEqual.js");
 
 
-console.log(head(["Earth", "Fire", "Water", "Air"]));
-console.log(head([1000, 200, 30, 4]));
+assertEqual(head(["Earth", "Fire", "Water", "Air"]), "Earth");
+assertEqual(head([1000, 200, 30, 4]), 200);
