a4974afbd70c54124fcba126b43444d37f84d143
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 22:18:17 -0800

    changed our test to utilize chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,19 @@
+const assert = require('chai').assert;
 const head = require("../head.js");
-const assertEqual = require("../assertEqual.js");
+
+describe("#head", () => {
+  it("should return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return 5 for [5]", () =>  {
+    assert.strictEqual(head([5]), 5);
+  });
+  it("should return Earth for [Earth, Fire, Water, Air]", () => {
+    assert.strictEqual(head(["Earth", "Fire", "Water", "Air"]), "Earth");
+  });
+  it("should return 1000 for [1000, 200, 30, 4]", () => {
+    assert.strictEqual(head([1000, 200, 30, 4]), 1000);
+  });
+});
 
 
-assertEqual(head(["Earth", "Fire", "Water", "Air"]), "Earth");
-assertEqual(head([1000, 200, 30, 4]), 200);
