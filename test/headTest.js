a4c7a8f4392d5070bc675c76f774f181374b8c08
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 22:23:02 -0800

    changed to test against type coercion

--- test/headTest.js
+++ test/headTest.js
@@ -5,8 +5,8 @@ describe("#head", () => {
   it("should return 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-  it("should return 5 for [5]", () =>  {
-    assert.strictEqual(head([5]), 5);
+  it("should return '5' for ['5']", () =>  {
+    assert.strictEqual(head(['5']), '5');
   });
   it("should return Earth for [Earth, Fire, Water, Air]", () => {
     assert.strictEqual(head(["Earth", "Fire", "Water", "Air"]), "Earth");
