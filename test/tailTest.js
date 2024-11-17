0cc5b1048b8f0c39dd9dcea6c10ffce7e5e95b6e
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 21:27:37 -0800

    made 4th cross-test more obvious

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,7 +7,7 @@ const tailArray = tail(arr);
 assertEqual(tailArray.length, 2);
 assertEqual(tailArray[0], "Sound");
 assertEqual(tailArray[1], "Brewery");
-assertEqual(tailArray[1], "Howe");
+assertEqual(tailArray[1], "Wrong");
 // checking to see if my array is still 3
 assertEqual(arr.length, 3);
 // testing to see if the empty array comes back an empty array
