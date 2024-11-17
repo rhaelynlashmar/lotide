791efc64ac914c19266f69ea73bbc0f4200ca957
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 20:30:02 -0800

    Added test for tail.js with assertEqual function

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require("../assertEqual.js");
+const tail = require("../tail.js");
+
+
+const arr = ["Howe", "Sound", "Brewery"];
+const tailArray = tail(arr);
+assertEqual(tailArray.length, 2);
+assertEqual(tailArray[0], "Sound");
+assertEqual(tailArray[1], "Brewery");
+assertEqual(tailArray[1], "Howe");
+// checking to see if my array is still 3
+assertEqual(arr.length, 3);
+// testing to see if the empty array comes back an empty array
+const emptyArr = tail([]);
+assertEqual(emptyArr.length, 0);
