2b43982abe9de8e96b0f16067b07b72b522b8071
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-16 19:15:29 -0800

    refactored to have testing in separate folder and added exporting links

--- assertEqual.js
+++ assertEqual.js
@@ -8,9 +8,4 @@ const assertEqual = function(actual, expected) {
 
 
 
-// console.log(assertEqual("Eat, Pray, Love", "Eat, Pray, Love"));
-// console.log(assertEqual(42, 42));
-// console.log(assertEqual(5 * 2, 10));
-// console.log(assertEqual(1000, "1000"));
-
 module.exports = assertEqual;
