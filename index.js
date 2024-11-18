749c012993367037a351fa14aa69ce7349abde51
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-17 16:58:25 -0800

    removed uneccessary functions and adjusted arrays typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArrayEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
@@ -10,9 +10,7 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -31,9 +29,7 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
-  flatten: flatten,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 };
