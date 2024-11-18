740ccc3af8fa55c93d5d6018bc2d1e87679f696d
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-17 16:35:20 -0800

    added export links

--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const eqArrays = require('./eqArrays');
 
 // create a function that takes in 2 objects and returns true or false, based on a perfect match
 const eqObjects = function(object1, object2) {
@@ -46,36 +26,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-
-
-const candyBag =  { type: "swedish berries", color: "pink", quantity: 30};
-const candyBag2 = { color: "pink", type: "swedish berries", quantity: 30};
-const candyBag3 = {type: "sour patch kids", quantity: 30};
-eqObjects(candyBag, candyBag2);
-eqObjects(candyBag, candyBag3);
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject, longSleeveShirtObject); // => false
-
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-assertEqual(eqObjects(candyBag, candyBag2), true);
-assertEqual(eqObjects(candyBag, candyBag3), false);
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
-
-const longSleeveMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-  sleeveLength: "long",
-};
-eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
-
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+module.exports = eqObjects;
