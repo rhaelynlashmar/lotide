6cfbdc6e6607a386f5f2574fe6a5958bc4396d11
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-17 16:38:34 -0800

    added export links

--- without.js
+++ without.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 // Create a without function which will return a subset of a given array, removing unwanted elements
 const without = function(source, itemsToRemove) {
   const result = [];
@@ -30,13 +9,17 @@ const without = function(source, itemsToRemove) {
   return result;
 };
 
-// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-// assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']);
+const assertArraysEqual = require('./assertArraysEqual');
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']);
 
 const candy = ["tootsie roll", "mars bar", "double bubble gum", "kit kat"];
-console.log(without(candy, ["tootsie roll", "double bubble gum"]));
 
-assertArraysEqual(candy, ["tootsie roll", "mars bar", "double bubble gum", "kit kat"]);
+assertArraysEqual(without(candy, ["mars bar"]), ["tootsie roll", "double bubble gum", "kit kat"]);
+assertArraysEqual(without(candy, ["mars bar", "double bubble gum"]), ["tootsie roll", "kit kat"]);
+
+module.exports = without;
 
 
 
