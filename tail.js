d8624700131d3c16509c1084cfbe8dbeeba02c60
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-17 16:38:06 -0800

    added export links

--- tail.js
+++ tail.js
@@ -1,6 +1,3 @@
-const assertEqual = require("./assertEqual");
-
-// Create a tail function
 const tail = function(array) {
   // remove the head of the array to only account for the tail
   return array.slice(1);
