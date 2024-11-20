073a84d11f8de841bd859b435a00e9d593e80db0
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-20 11:19:50 -0800

    assertArraysEqual testing commented out until proper testing is allocated

--- letterPositions.js
+++ letterPositions.js
@@ -19,9 +19,9 @@ const letterPositions = function(sentence) {
 
 const assertArraysEqual = require('./assertArraysEqual');
 
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
+// assertArraysEqual(letterPositions("hello").h, [0]);
+// assertArraysEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").l, [2, 3]);
+// assertArraysEqual(letterPositions("hello").o, [4]);
 
 module.exports = letterPositions;
