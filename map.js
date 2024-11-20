b4a56f037f8a08ed804f8e125bd71a6b3503ef7c
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-20 11:21:16 -0800

    assertArraysEqual testing commented out until proper testing is allocated

--- map.js
+++ map.js
@@ -10,11 +10,11 @@ const map = function(array, callback) {
 
 const assertArraysEqual = require('./assertArraysEqual');
 
-const words = ['all', 'right', 'then.', 'keep', 'your', 'secrets'];
-const colors = ["blue", "pink", "yellow", "green", "purple"];
+// const words = ['all', 'right', 'then.', 'keep', 'your', 'secrets'];
+// const colors = ["blue", "pink", "yellow", "green", "purple"];
 
-assertArraysEqual(map(words, (word) => word[0]), [ 'a', 'r', 't', 'k', 'y', 's' ]);
-assertArraysEqual(map(colors, (color) => color === "pink"), [ false, true, false, false, false ]);
-assertArraysEqual(map(colors, (color) => color.length > 4), [ false, false, true, true, true ]);
+// assertArraysEqual(map(words, (word) => word[0]), [ 'a', 'r', 't', 'k', 'y', 's' ]);
+// assertArraysEqual(map(colors, (color) => color === "pink"), [ false, true, false, false, false ]);
+// assertArraysEqual(map(colors, (color) => color.length > 4), [ false, false, true, true, true ]);
 
 module.exports = map;
