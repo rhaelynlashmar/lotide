7b78dd35b59c2451fe4f8ceb21831b22475fd586
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-20 11:13:48 -0800

    assertEqual testing commented out until proper testing is written

--- countOnly.js
+++ countOnly.js
@@ -36,9 +36,9 @@ const result1 = countOnly(firstNames, {
 
 const assertEqual = require('./assertEqual');
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
 
 module.exports = countOnly;
