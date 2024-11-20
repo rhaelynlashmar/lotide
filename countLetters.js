cb811adaaf92849e9c5b460b5887e07ceaede4da
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-20 11:12:48 -0800

    assertEqual testing  commented out until proper testing is written

--- countLetters.js
+++ countLetters.js
@@ -18,10 +18,10 @@ const countLetters = function(string) {
 
 const assertEqual = require('./assertEqual');
 
-assertEqual(countLetters("lighthouse in the house").l, 1);
-assertEqual(countLetters("lighthouse in the house").u, 2);
-assertEqual(countLetters("lighthouse in the house").h, 4);
-assertEqual(countLetters("lighthouse in the house").e, 3);
+// assertEqual(countLetters("lighthouse in the house").l, 1);
+// assertEqual(countLetters("lighthouse in the house").u, 2);
+// assertEqual(countLetters("lighthouse in the house").h, 4);
+// assertEqual(countLetters("lighthouse in the house").e, 3);
 
 module.exports = countLetters;
 
